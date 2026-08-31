a7e96aa6a2549634eb7e43f2aa5a9045fea24b93
less090604 <less.ms09@gmail.com>
2026-08-31 09:34:19 -0500

    Delete G1Array directory

--- G1Array/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module pe.edu.upeu.g1array {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens pe.edu.upeu.g1array to javafx.fxml;
-    exports pe.edu.upeu.g1array;
-}
